13d8905141c3e09c3cbcff2573004717b46d0743
oluwatobi ayoola jolaosho <fury25423@gmail.com>
2025-09-12 13:35:32 +0100

    added the down statement to create story book template -4

---
+++ migrations/20250912122300_add_unique_constraint_to_role_id_in_roles_table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.alterTable("roles", (table) => {
+    table.unique("role_id");
+  });
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema.alterTable("roles", (table) => {
+    table.dropUnique("role_id");
+  });
+}
